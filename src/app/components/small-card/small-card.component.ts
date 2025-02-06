4eb3c6c19d40ad43b400229f9253d1d2914e88cc
renandonun <renandonun@gmail.com>
2025-02-06 09:54:47 -0300

    content created

---
+++ src/app/components/small-card/small-card.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-small-card',
+  standalone: false,
+  
+  templateUrl: './small-card.component.html',
+  styleUrl: './small-card.component.css'
+})
+export class SmallCardComponent {
+  @Input()
+  photoCover: string = '';
+  @Input()
+  title: string = '';
+  @Input()
+  id: string = '0';
+}
---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+    {
+        "id": 1,
+        "photo": "https://cdn.pixabay.com/photo/2015/09/05/23/25/business-926221_960_720.jpg",
+        "title": "Desvendando o Angular: Guia Completo para Iniciantes",
+        "description": "Aprenda os fundamentos do framework, desde a criação de componentes até o gerenciamento de estados, e construa interfaces incríveis! Embarque em uma jornada de aprendizado completa e desvende os segredos do Angular, o framework JavaScript poderoso que impulsiona aplicações web modernas e dinâmicas. Este guia abrangente foi cuidadosamente elaborado para iniciantes, guiando você passo a passo desde os conceitos básicos até as técnicas avançadas de desenvolvimento."
+    },
+    {
+        "id": 2,
+        "photo": "https://cdn.pixabay.com/photo/2015/05/28/14/53/ux-788002_960_720.jpg",
+        "title": "CSS Moderno: Dicas e Truques para Estilos Impactantes",
+        "description": "Domine o CSS moderno e crie estilos web impressionantes! Explore dicas e truques avançados, desde layouts flexíveis e responsivos até animações e efeitos visuais de última geração. Aprenda a otimizar seu código CSS para melhor desempenho e acessibilidade, e descubra como usar pré-processadores e frameworks para agilizar seu fluxo de trabalho."
+    },
+    {
+        "id": 3,
+        "photo": "https://cdn.pixabay.com/photo/2017/10/10/21/47/laptop-2838921_960_720.jpg",
+        "title": "Node.js para Back-end: Construindo APIs Poderosas",
+        "description": "Desvende o poder do Node.js e construa APIs robustas e escaláveis para suas aplicações web. Aprenda a usar frameworks como Express para criar rotas, lidar com requisições e respostas, e conectar seu back-end a bancos de dados. Descubra como implementar autenticação, autorização e outras funcionalidades essenciais para suas APIs."
+    },
+    {
+        "id": 4,
+        "photo": "https://cdn.pixabay.com/photo/2017/10/01/14/56/communication-2805785_960_720.jpg",
+        "title": "Machine Learning para Iniciantes: Construindo Modelos Inteligentes",
+        "description": "Dê seus primeiros passos no mundo do Machine Learning e aprenda a construir modelos inteligentes que resolvem problemas do mundo real. Explore os principais algoritmos de aprendizado supervisionado e não supervisionado, e descubra como preparar seus dados, treinar seus modelos e avaliar seu desempenho."
+    }
+]
